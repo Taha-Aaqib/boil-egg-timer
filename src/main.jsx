b0b0e33fc5888725a4e9144f17284a0252784b0e
TAHA <m.tahaaaqib@gmail.com>
2025-10-27 00:38:55 +0500

    Uncomment NavBar import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 // import NavBar from './NavBar.jsx'
 
-import New from './New.jsx'
+
 import BoiledEggTimer from './BoiledEggTimer.jsx'
 
 createRoot(document.getElementById('root')).render(
